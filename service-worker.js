0c505eb6510f0cabc2094191cf11142c64d272aa
pokwerJ84 <pokwer1984@gmail.com>
2026-08-02 23:36:36 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'japan-protein-cookbook-v4';
+const CACHE = 'japan-protein-cookbook-v8-clean-design';
 const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./icons/icon-32.png','./icons/icon-180.png','./apple-touch-icon.png','./apple-touch-icon-precomposed.png','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
